b0dd3ab83bedeb4d4b6d6700720ae5bcc266bd1f
Diogenes <diogenesofweb@gmail.com>
2021-06-21 20:51:42 +0300

    init

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,40 @@
+<main>
+	<nav class="ce">
+		<a href="/">🇬🇧</a>
+		<a href="/fr">🇫🇷</a>
+		<a href="/ua">🇺🇦</a>
+	</nav>
+
+	<section>
+		<slot />
+	</section>
+</main>
+
+<style>
+	.ce {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 1rem;
+	}
+
+	a {
+		font-size: 2rem;
+		text-decoration: none;
+	}
+
+	section {
+		margin-top: 3rem;
+		text-align: center;
+	}
+
+	:global(body) {
+		font-size: 16px;
+	}
+
+	:global(h1) {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+			'Open Sans', 'Helvetica Neue', sans-serif;
+		font-size: clamp(2rem, 15vw, 8rem);
+	}
+</style>
---
+++ src/routes/fr/index.svelte
@@ -0,0 +1,13 @@
+<script context="module">
+	import { browser, dev } from '$app/env'
+
+	export const hydrate = dev
+	export const router = browser
+	export const prerender = true
+</script>
+
+<svelte:head>
+	<title>Bonjour</title>
+</svelte:head>
+
+<h1>Bonjour le Monde</h1>
